91ae9f40b632f3eacbf9cb5691a748a369f6fa03
solomonakp <solomonakp@gmail.com>
2021-02-17 22:24:36 +0100

    use theme done

--- components/useTheme.tsx
+++ components/useTheme.tsx
@@ -2,14 +2,65 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 interface Theme {
-  [key: string]: string | {} | ((param: string) => string);
+  fontFamily: {
+    headingFont: string;
+    textFont: string;
+  };
+  colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    iconColor: string;
+    cardHover: string;
+  };
+  spacing: {
+    sectionSpace: string;
+    blogCard: string;
+    blogCardSide: string;
+  };
+  size: {
+    mainHeading: string;
+    sectionHeading: string;
+    paragraph: string;
+    textHeading: string;
+  };
+  border: {
+    buttonBorder: string;
+  };
+  radius: {
+    button: string;
+    card: string;
+  };
+  shadow: {
+    contact: string;
+  };
+  effects: {
+    stretch: string;
+    buttonTransitionIn: string;
+    buttonTransitionOut: string;
+    allIn: (duration: number) => string;
+    allOut: (duration: number) => string;
+  };
+  breakPoints: {
+    minSm: string;
+    minMd: string;
+    minLg: string;
+    minXl: string;
+    minXxL: string;
+    maxXs: string;
+    maxSm: string;
+    maxMd: string;
+    maxLg: string;
+    maxXl: string;
+  };
+  // [key: string]: string | {} | ((param: string) => string);
 }
 
 const useTheme = () => {
   const theme: Theme = {
     fontFamily: {
       headingFont: 'Lufga',
-      textfont: 'Work Sans',
+      textFont: 'Work Sans',
     },
     colors: {
       primary: '#EF4E3C',
@@ -45,14 +96,25 @@ const useTheme = () => {
         'color 100ms ease-in, transform 80ms ease-in, background-color 100ms ease-in;',
       buttonTransitionOut:
         'color 100ms ease-out, transform 80ms ease-out,background-color 100ms ease-in',
-      allIn: (duration) => {
+      allIn: (duration: number): string => {
         return `all ${duration}ms ease-in`;
       },
-      allOut: (duration) => {
+      allOut: (duration: number): string => {
         return `all ${duration}ms ease-out`;
       },
     },
-    breakPoints: {},
+    breakPoints: {
+      minSm: 'min-width: 576px',
+      minMd: 'min-width: 768px',
+      minLg: 'min-width: 992px',
+      minXl: 'min-width: 1200px',
+      minXxL: 'min-width: 1400px',
+      maxXs: 'max-width: 575.98px',
+      maxSm: 'max-width: 767.98px',
+      maxMd: 'max-width: 991.98px',
+      maxLg: 'max-width: 1199.98px',
+      maxXl: 'max-width: 1399.98px',
+    },
   };
 
   return theme;
