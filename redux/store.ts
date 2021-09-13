27fe5757d530979d4a52ebef18b7eab51099ae42
solomonakp <solomonakp@gmail.com>
2021-09-13 10:52:05 +0100

    fixed store setup

--- redux/store.ts
+++ redux/store.ts
@@ -1,38 +1,41 @@
-import { createStore, applyMiddleware, AnyAction } from 'redux';
-import { HYDRATE, createWrapper, MakeStore, Context } from 'next-redux-wrapper';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, AnyAction, Store } from 'redux'
+import { HYDRATE, createWrapper } from 'next-redux-wrapper'
+import thunkMiddleware from 'redux-thunk'
 
 // imports index.js of reducers
-import rootReducer from './reducers';
-import { RootState } from './reducers/index';
+import rootReducer from './reducers'
+import { RootState } from './reducers/index'
 
 const bindMiddleware = (middleware) => {
   if (process.env.NODE_ENV !== 'production') {
-    const { composeWithDevTools } = require('redux-devtools-extension');
-    return composeWithDevTools(applyMiddleware(...middleware));
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
   }
-  return applyMiddleware(...middleware);
-};
+  return applyMiddleware(...middleware)
+}
 
-const combinedReducer = rootReducer;
+const combinedReducer = rootReducer
+
 const reducer = (state: RootState, action: AnyAction) => {
   if (action.type === HYDRATE) {
     const nextState = {
       ...state, // use previous state
       ...action.payload, // apply delta from hydration
-    };
+    }
 
-    return nextState;
+    return nextState
   } else {
-    return combinedReducer(state, action);
+    return combinedReducer(state, action)
   }
-};
+}
 
 // create a makeStore function
-const initStore: MakeStore<RootState> = (context: Context) => {
+const initStore = () => {
   // creating store and binding middlewares
-  return createStore(reducer, bindMiddleware([thunkMiddleware]));
-};
+  return createStore(reducer, undefined, bindMiddleware([thunkMiddleware]))
+}
 
 // export an assembled wrapper
-export const wrapper = createWrapper<RootState>(initStore, { debug: true });
+export const wrapper = createWrapper<Store<RootState>>(initStore, {
+  debug: true,
+})
