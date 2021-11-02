77867d57bb84b906d4dc12fc1887379fa9129f38
solomonakp <solomonakp@gmail.com>
2021-11-02 23:27:51 +0100

    fixed styling issue for mackdown components

--- components/postComponents/PostBody.tsx
+++ components/postComponents/PostBody.tsx
@@ -16,7 +16,6 @@ const PostBody = (props: PostBodyProps) => {
   return (
     <article className="post">
       <ReactMarkdown className="post-content">{content}</ReactMarkdown>
-
       <style jsx>{`
         .post {
           max-width: 90%;
@@ -24,27 +23,27 @@ const PostBody = (props: PostBodyProps) => {
             max-width: 100%;
           }
         }
-        :global(p) {
+        :global(.post-content p) {
           margin-bottom: 1.5rem;
         }
-        :global(ul) {
+        :global(.post-content ul) {
           list-style-type: none;
           color: ${dark};
           padding-left: 0;
-          :global(li) {
-            margin-left: 1.5rem;
-            position: relative;
-            &:before {
-              content: '';
-              position: absolute;
-              display: block;
-              border-radius: 50%;
-              width: 0.5em;
-              height: 0.5em;
-              left: -1.5em;
-              top: 8px;
-              background-color: ${dark};
-            }
+        }
+        :global(.post-content li) {
+          margin-left: 1.5rem;
+          position: relative;
+          &:before {
+            content: '';
+            position: absolute;
+            display: block;
+            border-radius: 50%;
+            width: 0.5em;
+            height: 0.5em;
+            left: -1.5em;
+            top: 8px;
+            background-color: ${dark};
           }
         }
       `}</style>
