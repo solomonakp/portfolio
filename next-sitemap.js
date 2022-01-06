66b07092d47dbd6ece7855b13309bf4e41ea24b0
solomonakp <solomonakp@gmail.com>
2022-01-06 13:30:37 +0100

    fixed enviroment variables

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,11 +1,12 @@
 // next-sitemap.js
+
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   exclude: ['/server-sitemap.xml'], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://example.com/server-sitemap.xml', // <==== Add here
+      `${process.env.SITE_URL}server-sitemap.xml`, // <==== Add here
     ],
   },
 }
