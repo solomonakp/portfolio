68e893e338884e2c5d167b20e67b06760d61984d
solomonakp <solomonakp@gmail.com>
2022-01-22 18:32:01 +0100

    updates to sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,10 +3,10 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'], // <= exclude here
+
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${process.env.SITE_URL}server-sitemap.xml`, // <==== Add here
+      `${process.env.SITE_URL}/server-sitemap.xml`, // <==== Add here
     ],
   },
 }
