210c504c3568fc3d663ac405080fd683097adc09
solomonakp <solomonakp@gmail.com>
2022-01-24 10:53:24 +0100

    removed serve side sitemaps entirely

--- next-sitemap.js
+++ next-sitemap.js
@@ -4,10 +4,4 @@ module.exports = {
   siteUrl: process.env.SITE_URL,
   changefreq: 'daily',
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'], // <= exclude here
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      `${process.env.SITE_URL}/server-sitemap.xml`, // <==== Add here
-    ],
-  },
 }
